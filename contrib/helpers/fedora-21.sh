7c0a25215f7c79afd645131d64b00d36d5fc186a
Richard Hughes <richard@hughsie.com>
2014-08-04 17:58:39 +0100

    trivial: Put the Fedora 21 metadata somewhere Kalev can get it

--- contrib/helpers/fedora-21.sh
+++ contrib/helpers/fedora-21.sh
@@ -7,7 +7,7 @@ time client/appstream-builder						\
 	--extra-appstream-dir=../fedora-appstream/appstream-extra	\
 	--extra-appdata-dir=../fedora-appstream/appdata-extra		\
 	--extra-screenshots-dir=../fedora-appstream/screenshots-extra	\
-	--output-dir=./contrib/metadata					\
+	--output-dir=./contrib/screenshots/fedora-21			\
 	--screenshot-dir=./contrib/screenshots/fedora-21		\
 	--basename=fedora-21						\
 	--screenshot-uri=http://alt.fedoraproject.org/pub/alt/screenshots/f21/
@@ -20,3 +20,8 @@ time client/appstream-builder						\
 ./client/appstream-util status-html 					\
 	./contrib/metadata/fedora-21-failed.xml.gz			\
 	./contrib/screenshots/fedora-21/failed.html
+
+# sync the screenshots and metadata
+cd contrib/screenshots/fedora-21/
+./upload-screenshots.sh
+cd -
--- contrib/screenshots/fedora-21/upload-screenshots.sh
+++ contrib/screenshots/fedora-21/upload-screenshots.sh
@@ -4,5 +4,9 @@ chmod a+rx 752x423
 chmod a+rx source
 chmod a+r */*.png
 chmod a+r *.html
-rsync -v --progress 112x63 624x351 752x423 *.html applications-to-import.yaml	\
+rsync -v --progress					\
+	112x63 624x351 752x423				\
+	fedora-21.xml.gz fedora-21-icons.tar.gz		\
+	*.html						\
+	applications-to-import.yaml			\
 	rhughes@secondary01.fedoraproject.org:/srv/pub/alt/screenshots/f21/
