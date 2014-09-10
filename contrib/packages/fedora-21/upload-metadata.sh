c02733cb4859d82653c7d91a5bde090ce475d493
Richard Hughes <richard@hughsie.com>
2014-09-10 09:27:37 +0100

    Move the distro helper scripts to another repo
    We don't want to clutter the git history with details of my RAID setup...

--- contrib/packages/fedora-21/upload-metadata.sh
+++
@@ -1,3 +0,0 @@
-scp fedora-*-icons.tar.gz hughsient@people.freedesktop.org:public_html/temp/
-scp fedora-*.xml.gz hughsient@people.freedesktop.org:public_html/temp/
-cp fedora-*-icons.tar.gz fedora-*.xml.gz $HOME/rpmbuild/SOURCES/
--- contrib/packages/fedora-21/upload-screenshots.sh
+++
@@ -1,3 +0,0 @@
-chmod a+rx screenshots/*x*
-chmod a+r screenshots/*x*/*.png
-rsync -v --progress ./screenshots/ rhughes@secondary01.fedoraproject.org:/srv/pub/alt/screenshots/f21/
