0a474d3c4445e1943dfe3a1dbf0f0f1140c8c434
Richard Hughes <richard@hughsie.com>
2016-04-27 11:48:15 +0100

    trivial: Add a work-around for the new libreoffice in the builder

--- libappstream-builder/asb-task.c
+++ libappstream-builder/asb-task.c
@@ -183,6 +183,11 @@ asb_task_explode_extra_packages (AsbTask *task, GError **error)
 			continue;
 		if (g_strcmp0 (tmp, asb_package_get_name (priv->pkg)) == 0)
 			continue;
+
+		/* libreoffice making things complicated */
+		if (g_strcmp0 (tmp, "libreoffice-core") == 0)
+			g_ptr_array_add (array, g_strdup ("libreoffice-data"));
+
 		/* if an app depends on kde-runtime, that means the
 		 * oxygen icon set is available to them */
 		if (g_strcmp0 (tmp, "oxygen-icon-theme") == 0 ||
