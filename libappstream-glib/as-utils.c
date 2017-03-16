5f9667a1ae80e74817db44ded9fd7f6696208149
Tim Chen <tim.chen119@canonical.com>
2017-03-16 15:02:52 +0800

    preserve 0x prefix in the version string less than 0xff

--- libappstream-glib/as-utils.c
+++ libappstream-glib/as-utils.c
@@ -1589,7 +1589,6 @@ as_utils_version_parse (const gchar *version)
 
 	/* convert 0x prefixed strings to dotted decimal */
 	if (g_str_has_prefix (version, "0x")) {
-		version += 2;
 		base = 16;
 	} else {
 		/* for non-numeric content, just return the string */
@@ -1601,7 +1600,7 @@ as_utils_version_parse (const gchar *version)
 	}
 
 	/* convert */
-	tmp = g_ascii_strtoull (version, &endptr, base);
+	tmp = g_ascii_strtoull (base == 16 ? version+2 : version, &endptr, base);
 	if (endptr != NULL && endptr[0] != '\0')
 		return g_strdup (version);
 	if (tmp == 0 || tmp < 0xff)
