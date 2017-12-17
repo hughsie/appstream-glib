cb98df0b008a5a1ccb4cdfa5e1ed945dc6c24d03
Richard Hughes <richard@hughsie.com>
2017-12-17 17:25:01 +0000

    Do not use an 'enable' prefix for meson arguments

--- contrib/ci/build_and_test.sh
+++ contrib/ci/build_and_test.sh
@@ -4,8 +4,8 @@ set -e
 mkdir -p build && cd build
 rm * -rf
 meson .. \
-    -Denable-gtk-doc=true \
-    -Denable-stemmer=true $@
+    -Dgtk-doc=true \
+    -Dstemmer=true $@
 ninja -v
 ninja test -v
 DESTDIR=/tmp/install-ninja ninja install
