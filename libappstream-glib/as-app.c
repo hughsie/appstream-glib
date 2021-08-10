700e499b376c9d8bb0aec15efd0604d3fcf42ec4
Matthias Klumpp <matthias@tenstral.net>
2021-08-10 16:55:09 +0200

    Don't emit legacy component type values

--- libappstream-glib/as-app.c
+++ libappstream-glib/as-app.c
@@ -138,7 +138,7 @@ const gchar *
 as_app_kind_to_string (AsAppKind kind)
 {
 	if (kind == AS_APP_KIND_DESKTOP)
-		return "desktop";
+		return "desktop-application";
 	if (kind == AS_APP_KIND_CODEC)
 		return "codec";
 	if (kind == AS_APP_KIND_FONT)
@@ -146,7 +146,7 @@ as_app_kind_to_string (AsAppKind kind)
 	if (kind == AS_APP_KIND_INPUT_METHOD)
 		return "inputmethod";
 	if (kind == AS_APP_KIND_WEB_APP)
-		return "webapp";
+		return "web-application";
 	if (kind == AS_APP_KIND_SOURCE)
 		return "source";
 	if (kind == AS_APP_KIND_ADDON)
